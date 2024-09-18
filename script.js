3336cc4016747a0566b53bd4057a4ebf667a26c1
craftlinks <90352073+craftlinks@users.noreply.github.com>
2024-09-18 13:53:27 +0200

    added prettier and eslint support

--- script.js
+++ script.js
@@ -1,117 +1,82 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
+import {
+    Fn,
+    uniform,
+    storage,
+    attribute,
+    float,
+    vec2,
+    vec3,
+    color,
+    instanceIndex,
+} from 'three/tsl'
 
-import { color, cos, float, mix, range, sin, timerLocal, uniform, uv, vec3, vec4, PI2 } from 'three';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
 
-import { GUI } from 'lil-gui';
-import { OrbitControls } from 'addons/controls/OrbitControls.js';
+let camera, scene, renderer
 
-let camera, scene, renderer, controls;
-
-init();
+init()
 
 function init() {
+    camera = new THREE.OrthographicCamera(-1.0, 1.0, 1.0, -1.0, 0, 1)
+    camera.position.z = 1
 
-    const canvas = document.querySelector('canvas.webgpu');
+    scene = new THREE.Scene()
 
-    camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 100 );
-    camera.position.set( 4, 2, 5 );
+    // use a compute shader to animate the point cloud's vertex data.
 
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0x201919 );
+    const particleNode = attribute('particle', 'vec2')
 
-    // galaxy
+    const pointsGeometry = new THREE.BufferGeometry()
+    pointsGeometry.setAttribute(
+        'position',
+        new THREE.BufferAttribute(new Float32Array(3), 3)
+    ) // single vertex ( not triangle )
+    pointsGeometry.setAttribute('particle', particleBuffer) // dummy the position points as instances
+    pointsGeometry.drawRange.count = 1 // force render points as instances ( not triangle )
 
-    const material = new THREE.SpriteNodeMaterial( {
-        transparent: true,
-        depthWrite: false,
-        blending: THREE.AdditiveBlending
-    } );
+    const pointsMaterial = new THREE.PointsNodeMaterial()
+    pointsMaterial.colorNode = particleNode.add(color(0xffffff))
+    pointsMaterial.positionNode = particleNode
 
-    const size = uniform( 0.03 );
-    material.scaleNode = range( 0.5, 1 ).mul( size );
+    const mesh = new THREE.Points(pointsGeometry, pointsMaterial)
+    mesh.count = particleNum
+    scene.add(mesh)
 
-    const time = timerLocal().mul(0.5);
+    renderer = new THREE.WebGPURenderer({ antialias: true })
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.setAnimationLoop(animate)
+    document.body.appendChild(renderer.domElement)
 
-    const radiusRatio = range( 0, 1 );
-    const radius = radiusRatio.pow( 1.5 ).mul( 2 ).toVar();
+    window.addEventListener('resize', onWindowResize)
+    window.addEventListener('mousemove', onMouseMove)
 
-    const branches = 120;
-    const branchAngle = range( 0, branches ).floor().mul( PI2.div( branches ) );
-    const angle = branchAngle.add( time.mul( radiusRatio.oneMinus() ) );
+    // gui
 
-    const randomOffset = range( vec3( - 1 ), vec3( 1 ) ).pow( 3 ).mul( radiusRatio ).add( 0.2 );
-    
-    const position = vec3(
-        cos( angle ),
-        0,
-        sin( angle )
-    ).mul( radius );
+    const gui = new GUI()
 
-    
-
-    material.positionNode = position.add( randomOffset );
-
-    const colorInside = uniform( color( '#ff7775' ) );
-    const colorOutside = uniform( color( '#331599' ) );
-    const colorFinal = mix( colorInside, colorOutside, radiusRatio.oneMinus().pow( 2 ).oneMinus() );
-    const alpha = float( 0.1 ).div( uv().sub( 0.5 ).length()).sub( 0.5 );
-    material.colorNode = vec4( colorFinal, alpha );
-
-    const mesh = new THREE.InstancedMesh( new THREE.PlaneGeometry( 1, 1 ), material, 200000 );
-    scene.add( mesh );
-
-    // debug
-
-    const gui = new GUI();
-
-    gui.add( size, 'value', 0, 1, 0.001 ).name( 'size' );
-
-    gui.addColor( { color: colorInside.value.getHex( THREE.SRGBColorSpace ) }, 'color' )
-        .name( 'colorInside' )
-        .onChange( function ( value ) {
-
-            colorInside.value.set( value );
-
-        } );
-
-    gui.addColor( { color: colorOutside.value.getHex( THREE.SRGBColorSpace ) }, 'color' )
-        .name( 'colorOutside' )
-        .onChange( function ( value ) {
-
-            colorOutside.value.set( value );
-
-        } );
-
-    // renderer
-
-    renderer = new THREE.WebGPURenderer( { canvas: canvas, antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.setAnimationLoop( animate );
-    document.body.appendChild( renderer.domElement );
-
-    controls = new OrbitControls( camera, renderer.domElement );
-    controls.enableDamping = true;
-    controls.minDistance = 0.1;
-    controls.maxDistance = 50;
-
-    window.addEventListener( 'resize', onWindowResize );
-
+    gui.add(scaleVector, 'x', 0, 1, 0.01)
+    gui.add(scaleVector, 'y', 0, 1, 0.01)
 }
 
 function onWindowResize() {
+    camera.updateProjectionMatrix()
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
+    renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
-async function animate() {
+function onMouseMove(event) {
+    const x = event.clientX
+    const y = event.clientY
 
-    controls.update();
+    const width = window.innerWidth
+    const height = window.innerHeight
 
-    renderer.render( scene, camera );
+    pointerVector.set((x / width - 0.5) * 2.0, (-y / height + 0.5) * 2.0)
+}
 
+function animate() {
+    renderer.compute(computeNode)
+    renderer.render(scene, camera)
 }
